65a4bca71c9f2bb48747d37a351bceac13f1a9a1
Huỳnh Ngọc Hưng <90973416+ngochung130201@users.noreply.github.com>
2024-03-28 16:07:21 +0700

    Update OrderRepository.cs

--- ecommerce/ecommerce/Repository/OrderRepository.cs
+++ ecommerce/ecommerce/Repository/OrderRepository.cs
@@ -1,15 +1,19 @@
-﻿using ecommerce.Middleware;
+﻿using ecommerce.Context;
+using ecommerce.Middleware;
 using ecommerce.Models;
 using ecommerce.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace ecommerce.Repository
 {
     public class OrderRepository : IOrderRepository
     {
         private readonly IRepositoryBase<Order> _repositoryBase;
-        public OrderRepository(IRepositoryBase<Order> repositoryBase)
+        private readonly EcommerceContext _context;
+        public OrderRepository(IRepositoryBase<Order> repositoryBase, EcommerceContext context)
         {
             _repositoryBase = repositoryBase;
+            _context = context;
         }
         public async Task<int> AddOrderAsync(Order order)
         {
@@ -55,13 +59,24 @@ namespace ecommerce.Repository
 
         public async Task<Order> GetOrderByIdAsync(int id)
         {
-            var order = await _repositoryBase.FindByIdAsync(id);
+            var order = await _context.Orders.Include(u => u.OrderItems).Include(x => x.User).ThenInclude(x => x.Carts).ThenInclude(u => u.CartItems).FirstOrDefaultAsync(x => x.OrderId == id);
             if (order == null)
             {
                 throw new CustomException("Order not found", 404);
             }
             return order;
         }
+
+        public async Task<Order> GetOrderByUserIdAsync(int userId)
+        {
+            var order = await _context.Orders.Include(u => u.OrderItems).Include(x => x.User).ThenInclude(x => x.Carts).ThenInclude(u => u.CartItems).FirstOrDefaultAsync(x => x.UserId == userId);
+            if (order != null)
+            {
+                return order;
+            }
+            return null;
+        }
+
 
         public async Task UpdateOrderAsync(int id, Order orderToUpdate)
         {
